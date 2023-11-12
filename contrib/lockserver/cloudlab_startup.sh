18d94b041d4ce8aab5a6cb6514fcfd52ae9cd0ce
Ananya Jain <ananyaj74@gmail.com>
2023-11-12 14:00:30 -0500

    Add first version of Cloudlab startup script

---
+++ contrib/lockserver/cloudlab_startup.sh
@@ -0,0 +1,9 @@
+# install Go
+curl -OL https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.21.4.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+go version
+
+rm go1.21.4.linux-amd64.tar.gz
