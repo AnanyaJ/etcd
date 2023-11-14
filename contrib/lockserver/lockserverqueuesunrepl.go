f6a2b8c543fc606dcf646cdcddbff5677496db80
Ananya Jain <ananyaj74@gmail.com>
2023-11-14 12:48:05 -0500

    Add unreplicated version of queue impl

---
+++ contrib/lockserver/lockserverqueuesunrepl.go
@@ -0,0 +1,85 @@
+package main
+
+type UnreplQueueLockServer struct {
+	proposeC  chan []byte
+	opManager *OpManager
+	locks     map[string]*LockQueue[LockOp]
+}
+
+// Replicated lock service built using a queue for each lock. This lock server
+// performs similarly to the one that uses condition variables, except that it can
+// safely be replicated since it uses queues to deterministically schedule
+// the Acquire operations. Currently uses a WAL that is persisted on disk, and
+// supports snapshotting.
+func newUnreplQueueLockServer() *UnreplQueueLockServer {
+	proposeC := make(chan []byte)
+	s := &UnreplQueueLockServer{
+		proposeC:  proposeC,
+		opManager: newOpManager(),
+		locks:     make(map[string]*LockQueue[LockOp]),
+	}
+	// apply commits from raft until error
+	go s.applyProposals()
+	return s
+}
+
+func (s *UnreplQueueLockServer) startOp(opType int, lockName string, clientID ClientID, opNum int64) bool {
+	op := LockOp{OpType: opType, LockName: lockName, ClientID: clientID, OpNum: opNum}
+	result := s.opManager.addOp(opNum)
+	s.proposeC <- op.marshal()
+	return <-result
+}
+
+func (s *UnreplQueueLockServer) Acquire(lockName string, clientID ClientID, opNum int64) {
+	s.startOp(AcquireOp, lockName, clientID, opNum)
+}
+
+func (s *UnreplQueueLockServer) Release(lockName string, clientID ClientID, opNum int64) bool {
+	return s.startOp(ReleaseOp, lockName, clientID, opNum)
+}
+
+func (s *UnreplQueueLockServer) IsLocked(lockName string, clientID ClientID, opNum int64) bool {
+	return s.startOp(IsLockedOp, lockName, clientID, opNum)
+}
+
+func (s *UnreplQueueLockServer) applyProposals() {
+	for proposedOp := range s.proposeC {
+		op := lockOpFromBytes(proposedOp)
+		s.addLock(op.LockName)
+		lock := s.locks[op.LockName]
+
+		switch op.OpType {
+		case AcquireOp:
+			if lock.IsLocked {
+				lock.Queue = append(lock.Queue, op)
+			} else {
+				lock.IsLocked = true
+				s.opManager.reportOpFinished(op.OpNum, true)
+			}
+		case ReleaseOp:
+			if !lock.IsLocked {
+				s.opManager.reportOpFinished(op.OpNum, false) // lock already free
+			}
+
+			if len(lock.Queue) > 0 {
+				// pass lock to next waiting op
+				unblocked := lock.Queue[0]
+				lock.Queue = lock.Queue[1:]
+				s.opManager.reportOpFinished(unblocked.OpNum, true)
+			} else {
+				lock.IsLocked = false
+			}
+
+			s.opManager.reportOpFinished(op.OpNum, true)
+		case IsLockedOp:
+			s.opManager.reportOpFinished(op.OpNum, lock.IsLocked)
+		}
+	}
+}
+
+func (s *UnreplQueueLockServer) addLock(lockName string) {
+	if s.locks[lockName] != nil {
+		return // already exists
+	}
+	s.locks[lockName] = &LockQueue[LockOp]{Queue: []LockOp{}}
+}
--- contrib/lockserver/main.go
+++ contrib/lockserver/main.go
@@ -26,7 +26,7 @@ func main() {
 	id := flag.Int("id", 1, "node ID")
 	lsport := flag.Int("port", 12380, "lock server port")
 	join := flag.Bool("join", false, "join an existing cluster")
-	impl := flag.String("impl", "blockingraft", "lock server implementation to use (simple, condvar, unreplcoros, queues, coros, kvlocks, blockingraft, kv)")
+	impl := flag.String("impl", "blockingraft", "lock server implementation to use (simple, condvar, unreplcoros, unreplqueues, queues, coros, kvlocks, blockingraft, kv)")
 	clearLog := flag.Bool("clearlog", false, "whether to use a fresh log, removing all previous persistent state")
 	timeout := flag.Int("timeout", 100, "number of milliseconds simple lock server waits between acquire attempts")
 	flag.Parse()
@@ -44,6 +44,9 @@ func main() {
 		serveHTTPLSAPI(lockServer, *lsport, confChangeC, make(<-chan error))
 	case "condvar":
 		lockServer := newCondVarLockServer()
+		serveHTTPLSAPI(lockServer, *lsport, confChangeC, make(<-chan error))
+	case "unreplqueues":
+		lockServer := newUnreplQueueLockServer()
 		serveHTTPLSAPI(lockServer, *lsport, confChangeC, make(<-chan error))
 	case "unreplcoros":
 		lockServer := newUnreplCoroLockServer()
