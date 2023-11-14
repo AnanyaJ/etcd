d8efbc7bfc58e86f66ed4af0818c9d43152d4955
Ananya Jain <ananyaj74@gmail.com>
2023-11-14 15:50:44 -0500

    Fix CloudLab script

--- contrib/lockserver/cloudlab_startup.sh
+++ contrib/lockserver/cloudlab_startup.sh
@@ -1,13 +1,9 @@
 # install Go
 curl -OL https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.21.4.linux-amd64.tar.gz
-
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
-. ~/.bashrc
-
-go version
-
 rm go1.21.4.linux-amd64.tar.gz
 
-# build code
-go build -o lockserver
+export PATH=\$PATH:/usr/local/go/bin
+hash -r
+
+go version
