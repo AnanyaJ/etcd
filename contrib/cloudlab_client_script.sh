a80b0aec8031ea588a7f8d0dba7c98033cedb967
Ananya Jain <ananyaj74@gmail.com>
2023-11-27 09:44:58 -0500

    Fix CloudLab scripts

--- contrib/cloudlab_client_script.sh
+++ contrib/cloudlab_client_script.sh
@@ -3,7 +3,7 @@ curl -OL https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.21.4.linux-amd64.tar.gz
 rm go1.21.4.linux-amd64.tar.gz
 
-export PATH=\$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/go/bin
 hash -r
 
 go version
